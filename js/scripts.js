afc724a99593ec11541fff8874d74e6ca8c21c50
dmcclelland <dmcclelland@ptc.com>
2018-07-10 21:32:10 -0400

    moved flexbox twx POC into this project so I can create classes and so forth in ES6

---
+++ js/scripts.js
@@ -0,0 +1,148 @@
+'use strict';
+
+// simple test to verify test is set up right
+function greet(name) {
+    return 'Hello, ' + name + '!';
+}
+
+$getJSON('ide', 'docks');
+var prefWidth = 0;
+
+function enableDocks(data) {
+    // return data for processing
+    var docks = data.docks,
+        i = 0;
+    for (i = 0; i < docks.length; i++) {
+        // set dock titles
+        $('#' + docks[i].id + ' .title').text(docks[i].name);
+        // populate dock with sections
+        var sections = docks[i]['sections'];
+        // are there preferences set?
+        if (sections.user !== null) {
+            //console.log(sections.user.length);
+            sections = sections.user;
+        } else {
+            sections = sections.default;
+        }
+
+        $('#dock1').resizable({
+            helper: "ui-resizable-helper",
+            containment: "parent",
+            handles: 'e',
+            resize: function resize(event, ui) {
+                $(this).css('height', '');
+                $(this).css('left', '');
+            },
+            stop: function stop(event, ui) {
+                $(this).css('height', '');
+                $(this).css('left', '');
+                $setPrefs(ui.size.width);
+                //w2 = Math.min(w2, 300);
+                // $('.dock').css('width', w2);
+            }
+        });
+
+        $('#layout').resizable({
+            helper: "ui-resizable-helper",
+            containment: "parent",
+            handles: 's',
+            resize: function resize(event, ui) {
+                $(this).css('width', '');
+                $(this).css('left', '');
+            },
+            stop: function stop(event, ui) {
+                $(this).css('width', '');
+                $(this).css('left', '');
+                $(this).css('height', ui.size.height);
+            }
+        });
+
+        $('#dock2').resizable({
+            helper: "ui-resizable-helper",
+            containment: "parent",
+            handles: 'w',
+            resize: function resize(event, ui) {
+                $(this).css('height', '');
+                $(this).css('left', '');
+            },
+            stop: function stop(event, ui) {
+                $(this).css('height', '');
+                $(this).css('left', '');
+                $(this).css('width', ui.size.width + $(this).offset().left - event.pageX);
+            }
+        });
+
+        // drag/drop support
+        $('.dock').show().droppable({
+            accept: '.section', hoverClass: 'dragging', tolerance: "touch",
+
+            drop: function drop(e, ui) {
+                // move element from one dock to the other
+                // id the dragged element
+                // console.log("ui: " + ui.draggable.id);
+                // id the drop target element
+                //console.log("e: " + e.target.id);
+                $(ui.draggable).appendTo($(this));
+                //$(this).find("h2").text("Dropped");
+                // ui.draggable.find("h2").text("Dropped");
+            }
+
+        });
+
+        // get saved dock width pref
+        console.log("prefWidth: " + docks[i].id + ", " + prefWidth);
+        $('#docks1.id').css('width', prefWidth);
+        enableSections(sections);
+    }
+}
+
+function enableSections(sections) {
+    var i = 0,
+        panels = void 0;
+    for (i = 0; i < sections.length; i++) {
+        // console.log(sections[i].name);
+        $('#' + sections[i].id).text(sections[i].name);
+        // populate dock with sections
+        var _panels = sections[i]['panels'];
+        // are there preferences set?
+        if (_panels.user !== null) {
+            //    console.log(panels.user.length);
+            _panels = _panels.user;
+        } else {
+            //     console.log("panels.default:" + panels.default);
+            _panels = _panels.default;
+        }
+
+        $('.section').draggable({ revert: true });
+
+        enablePanels(_panels);
+    }
+}
+
+function enablePanels(panels) {
+    var i = 0;
+    for (i = 0; i < panels.length; i++) {
+        $('#' + panels[i].id).text(panels[i].name);
+    }
+}
+
+function $getJSON(api, requestedData) {
+    $.ajax({
+        url: "http://localhost:3002/" + api,
+        type: "GET",
+        contentType: "application/json",
+        data: null,
+        dataType: "json",
+        cache: false
+    }).done(function (data) {
+        $getPrefs();
+        enableDocks(data);
+        // can also use fit, intersect, pointer
+        return data[requestedData];
+    }).fail(function (xhr, status) {
+        console.log("error");
+    }).always(function (xhr, status) {
+        // reset things
+
+    });
+}
