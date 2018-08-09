9f14179dfd729fab88f32837d25e797b342f18e6
dmcclelland <dmcclelland@ptc.com>
2018-08-09 14:09:28 -0400

    added some examples that output to console let in function let in for loop arrow expression body example

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+"use strict";
+
 let calculateMonthlyPayment = function (principal, years, rate) {
     // function level let needs to be initialized in the function not in the if statement
     let monthlyRate = 0;
@@ -16,3 +18,37 @@ document.getElementById('calcBtn').addEventListener('click', function () {
     let monthlyPayment = calculateMonthlyPayment(principal, years, rate);
     document.getElementById("monthlyPayment").innerHTML = monthlyPayment.toFixed(2);
 });
+
+// a much clearer example:
+
+var submit = function () {
+    var x = "foo";
+    let y;
+    if (x === "foo") {
+        // let y = "bar"; // undefined because let scope is inside brackets
+        y = "bar";
+    }
+    console.log(x);
+    console.log(y);
+};
+
+submit();
+
+
+// in for loops:
+
+let shoppingCart = [{id: 0, product: "DVD", price: 21.99},
+    {id: 1, product: "CD", price: 11.99}];
+
+for (let i = 0; i < shoppingCart.length; i++) {
+    let item = shoppingCart[i];
+    console.log("Item: " + item.product + " - Price: " + item.price);
+}
+
+// arrow expression body
+
+let evens = [2, 4, 6, 8, 10];
+let odds = evens.map(v => v + 1);
+let nums = evens.map((v, i) => v + i);
+console.log(odds);
+console.log(nums);
--- readme.md
+++ readme.md
@@ -7,7 +7,7 @@ This branch tracks [here](https://ccoenraets.github.io/es6-tutorial/let/).
 Replacing var with let potentially changes scopes within functions and can cause transpiled
 scripts to break. This lesson demonstrates this and shows how to detect and fix instances.
 
-This branch will show the following errir in Chrome console when run:
+This branch will show the following error in Chrome console when run:
 
 `main.bundle.js:7 Uncaught ReferenceError: monthlyRate is not defined
      at calculateMonthlyPayment (main.bundle.js:7)
